355ecf5476fda71ce97a18345a7d1861ed1be929
nsrstory <76632542+syahrisyah68@users.noreply.github.com>
2021-06-14 11:39:03 +0700

    Add files via upload

---
+++ language/ind.js
@@ -0,0 +1,17 @@
+const a = '```'
+
+exports.wrongf = () => {
+	return`*format salah/text kosong*`
+}
+
+exports.noregis = () => {
+	return`*「 BELUM DAFTAR 」*\n\n*cara daftar ${prefix}daftar nama|umur* \n*contoh ${prefix}daftar ZHARZXCHANNEL|16*`
+}
+
+exports.rediregis = () => {
+	return`*「 SUDAH DAFTAR 」*\n\n*kamu sudah terdaftar di database bot*`
+}
+
+exports.registered = (namaUser, umurUser, serialUser, time, sender) => {
+	return`*「 DATA ZX BOT」*\n\nkamu sudah terdaftar dengan data \n\n┏━⊱nama\n┗⊱${namaUser}\n┏━⊱nomer\n┗⊱wa.me/${sender.split("@")[0]}\n┏━⊱umur\n┗⊱${umurUser}\n┏━⊱waktu pendaftaran\n┗⊱${time}\n\n┏━❉ *NS* ❉━\n┣⊱${serialUser}\n┗⊱NOTE : JANGAN LUPA SUBS CHANNEL https://youtube.com/channel/UCpsA_nCZZ87CIjM6JHoi3sg`
+}
